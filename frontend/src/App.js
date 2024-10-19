6d902081b5ec6a42b86eb024d7122175cb551d34
Liza Tinku Jose <ltinkujose@ucdavis.edu>
2024-10-19 00:19:59 -0700

    pulse animation removed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import AIChat from './components/AIChat';
 import MoodCalendar from './components/MoodCalendar';
-import PulseAnimation from './components/PulseAnimation';
+//import PulseAnimation from './components/PulseAnimation';
 
 function App() {
 
@@ -24,7 +24,6 @@ function App() {
           <Route path="/chat" element={<AIChat />} />
           <Route path="/calendar" element={<MoodCalendar />} />
           <Route path="/" element={<AIChat />} /> 
-          <Route path="/" element={<PulseAnimation />} />
         </Routes>
       </div>
     </Router>
